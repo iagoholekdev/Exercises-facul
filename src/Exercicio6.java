26d1c8854d69411da7b77562fc1cf18875310bef
iagoholekdev <augustinhocarrara43@gmail.com>
2023-03-27 23:45:54 -0300

    Exercicio 6

---
+++ src/Exercicio6.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Exercicio6 {
+
+    static double convertCelsiusToFahrenheit(int number) {
+        final double calculus = 1.8;
+        final int sumNumber   = 32;
+        return (number * calculus) + sumNumber;
+    }
+    public static void main(String[] args){
+       // . Elaborar um programa que apresente os valores de conversão de graus Celsius
+        //em graus Fahrenheit, de dez em dez graus, iniciando a contagem em dez graus
+      //  Celsius e finalizando em cem graus Celsius. O programa deve apresentar os
+       // valores das duas temperaturas. Para converter grau Celsius para Fahrenheit, basta
+       // multiplicar a temperatura em graus Celsius por 1,8 e somar 32
+        final int lastCount = 100;
+        final int plusTen   = 10;
+        Map<String,Integer> celsius   = new HashMap<String,Integer>();
+        Map<String,Double> Fahrenheit = new HashMap<String,Double>();
+        for (int count = 10; count <= lastCount; count+=plusTen) {
+          celsius.put("Celsius: ", count);
+          System.out.println(celsius);
+          Fahrenheit.put("Fahrenheit: ", Exercicio6.convertCelsiusToFahrenheit(count));
+          System.out.println(Fahrenheit);
+        }
+    }
+}
